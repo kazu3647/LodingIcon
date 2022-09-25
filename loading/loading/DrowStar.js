663d88d2f653cac0df2e9dc81eb14f947ad7069c
kazu3647 <69095400+kazu3647@users.noreply.github.com>
2022-09-26 02:00:05 +0900

    Add files via upload

---
+++ loading/loading/DrowStar.js
@@ -0,0 +1,43 @@
+onload = function() {
+    draw();
+};
+
+var cvs;
+var length = 30;
+var radius = 100;
+var center = [100, 300];
+// 5つの点が参照できる
+// 現在地から線を引いて移動させる
+// 移動した場所の長さが未満なら、次の点への残りの線を引く
+
+
+function draw(){
+    var canvas = document.getElementById('DrowStar');
+    if (!canvas || !canvas.getContext) {
+        return false;
+    }
+    cvs = canvas.getContext('2d');
+
+    // starの描画をする(黒)
+    cvs.clearRect(0, 200, 200, 400);
+    cvs.beginPath();
+    for (let i = 0; i <= 4*Math.PI; i += 4*Math.PI/5){
+        cvs.lineTo(
+            center[0] + radius * Math.sin(i),
+            center[0] - radius * Math.cos(i)
+        );
+    }
+    cvs.stroke();
+
+    // ここから描画を開始
+    window.requestAnimationFrame((ts) => loop(ts));
+}
+
+function loop(){
+    // starをなぞり続ける(赤)
+    cvs.beginPath();
+    cvs.strokeStyle = 'red';
+
+
+    window.requestAnimationFrame((ts) => loop(ts))
+}
